d852c32b5fbb91efc314983e5722cd90c64d2f39
Alexey Zhokhov <alexey@zhokhov.com>
2019-07-26 18:45:51 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,37 @@ chmod 600 /dev/net/tun
 
 set -e
 
+export DOCKER_VERSION=`docker --version`
+export REDIS_VERSION=`redis-server --version`
+export POSTGRESQL_VERSION=`psql --version`
+export RABBITMQ_VERSION=`rabbitmqctl version`
+export MONGODB_VERSION=`mongod --version`
+export NODEJS_VERSION=`node --version`
+
+echo "DOCKER"
+echo ${DOCKER_VERSION}
+echo ""
+
+echo "REDIS"
+echo ${REDIS_VERSION}
+echo ""
+
+echo "POSTGRESQL"
+echo ${POSTGRESQL_VERSION}
+echo ""
+
+echo "RABBITMQ"
+echo ${RABBITMQ_VERSION}
+echo ""
+
+echo "MONGODB"
+echo ${MONGODB_VERSION}
+echo ""
+
+echo "NODEJS"
+echo ${NODEJS_VERSION}
+echo ""
+
 if [ "$DNS_SERVER_IP" ]; then
     echo "nameserver ${DNS_SERVER_IP}" > /etc/resolv.conf
 
