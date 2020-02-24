f8fe2c61ac05ff2a95e3aa42d9f7aa10ffa1a5c8
Alexey Zhokhov <alexey@zhokhov.com>
2020-02-25 01:05:09 +0800

    Fixed Google Chrome installation fixes.

--- scripts/install_google_chrome.sh
+++ scripts/install_google_chrome.sh
@@ -9,6 +9,8 @@ apt-get install -y \
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
+/scripts/update.sh
+
 apt-get install -y \
   google-chrome-stable
 
